fca6409bae8acbe1a3b64cc2902342734d4447d9
panthevm <panthevm@yandex.ru>
2021-12-23 19:59:59 +0300

    [SCHEME] Graph solution update

--- scheme/graph/graph.scm
+++ scheme/graph/graph.scm
@@ -11,7 +11,9 @@
        (2))))))
 
 (define (solution accumulator node)
-  (+ accumulator (/ (+ (car node) (fold-left solution 0 (cdr node))) 2)))
+  (+ accumulator
+     (car node)
+     (/ (fold-left solution 0 (cdr node)) 2.0)))
 
 (newline)
-(display (* (solution 0 tree) 2.0))
+(display (solution 0 tree))
